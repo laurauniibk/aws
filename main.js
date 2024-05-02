81a122bc518dbd3d7dcfc6847f2e3906f430ea8d
laurauniibk <l.eddelbuettel@hotmail.com>
2024-05-02 16:13:59 +0200

    add feature group for temperature and function for l.geojson

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ let map = L.map("map", {
 // thematische Layer
 let themaLayer = {
     stations: L.featureGroup().addTo(map),
+    temperature: L.featureGroup().addTo(map),
 }
 
 // Hintergrundlayer
@@ -26,13 +27,29 @@ L.control.layers({
     "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
     "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery")
 }, {
-    "Wetterstationen": themaLayer.stations
+    "Wetterstationen": themaLayer.stations,
+    "Temperatur": themaLayer.temperature,
 }).addTo(map);
 
 // Maßstab
 L.control.scale({
     imperial: false,
 }).addTo(map);
+
+//ShowTemperature definieren und Marker Temperatur 
+function showTemperature(geojson) {
+    L.geoJSON(geojson, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: L.divIcon({
+                    className: "aws-div-icon",
+                    html: `<span>${feature.properties.LT}</span>`
+                }
+                )
+            })
+        }
+    }).addTo(themaLayer.temperature);
+}
 
 // GeoJSON der Wetterstationen laden
 async function showStations(url) {
@@ -68,6 +85,6 @@ async function showStations(url) {
             `);
         }
     }).addTo(themaLayer.stations);
-
+    showTemperature(geojson);
 }
 showStations("https://static.avalanche.report/weather_stations/stations.geojson");
